c1a88b3d3fd83a6e3c41694eb5552492628659b4
Kazim <kazimgumus032@gmail.com>
2021-06-10 16:58:53 +0300

    #15 Şoförler sayfası başlangıç

--- Taksi Uygulamasi/Taksi Uygulamasi/Soforler.Designer.cs
+++ Taksi Uygulamasi/Taksi Uygulamasi/Soforler.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.No = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Adı = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Soyadı = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Yaş = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Mail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Telefon = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Ehliyet = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Kayıt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.No,
+            this.Adı,
+            this.Soyadı,
+            this.Yaş,
+            this.Mail,
+            this.Telefon,
+            this.Ehliyet,
+            this.Kayıt});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(-4, 299);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(803, 151);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // No
+            // 
+            this.No.Text = "No";
+            // 
+            // Adı
+            // 
+            this.Adı.Text = "Adı";
+            this.Adı.Width = 79;
+            // 
+            // Soyadı
+            // 
+            this.Soyadı.Text = "Soyadı";
+            this.Soyadı.Width = 101;
+            // 
+            // Yaş
+            // 
+            this.Yaş.Text = "Yaş";
+            // 
+            // Mail
+            // 
+            this.Mail.Text = "Mail";
+            this.Mail.Width = 141;
+            // 
+            // Telefon
+            // 
+            this.Telefon.Text = "Telefon";
+            this.Telefon.Width = 123;
+            // 
+            // Ehliyet
+            // 
+            this.Ehliyet.Text = "Ehliyet";
+            this.Ehliyet.Width = 95;
+            // 
+            // Kayıt
+            // 
+            this.Kayıt.Text = "Kayıt";
+            this.Kayıt.Width = 153;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Soforler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Soforler";
             this.Text = "Soforler";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader No;
+        private System.Windows.Forms.ColumnHeader Adı;
+        private System.Windows.Forms.ColumnHeader Soyadı;
+        private System.Windows.Forms.ColumnHeader Yaş;
+        private System.Windows.Forms.ColumnHeader Mail;
+        private System.Windows.Forms.ColumnHeader Telefon;
+        private System.Windows.Forms.ColumnHeader Ehliyet;
+        private System.Windows.Forms.ColumnHeader Kayıt;
+        private System.Windows.Forms.Button button1;
     }
 }
