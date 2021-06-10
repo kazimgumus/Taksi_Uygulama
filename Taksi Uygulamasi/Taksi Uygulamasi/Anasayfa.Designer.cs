c1529635cdb2c2e89f715124884194a6074a7192
Kazim <kazimgumus032@gmail.com>
2021-06-10 12:53:01 +0300

    #10 anasayfaya çıkış butonu eklendi

--- Taksi Uygulamasi/Taksi Uygulamasi/Anasayfa.Designer.cs
+++ Taksi Uygulamasi/Taksi Uygulamasi/Anasayfa.Designer.cs
@@ -28,76 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            System.Windows.Forms.Button button1;
+            System.Windows.Forms.Button button2;
+            System.Windows.Forms.Button button3;
+            System.Windows.Forms.Button button4;
+            System.Windows.Forms.Button button5;
+            System.Windows.Forms.Button button6;
+            this.button7 = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(42, 53);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 81);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Admin Giriş";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            button1.Location = new System.Drawing.Point(42, 53);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(121, 81);
+            button1.TabIndex = 0;
+            button1.Text = "Admin Giriş";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button2.Location = new System.Drawing.Point(317, 53);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 81);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Yeni Kayıt";
-            this.button2.UseVisualStyleBackColor = true;
+            button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            button2.Location = new System.Drawing.Point(317, 53);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(121, 81);
+            button2.TabIndex = 1;
+            button2.Text = "Yeni Kayıt";
+            button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button3.Location = new System.Drawing.Point(573, 53);
-            this.button3.Name = "button3";
-            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.button3.Size = new System.Drawing.Size(121, 81);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Araçlar";
-            this.button3.UseVisualStyleBackColor = true;
+            button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            button3.Location = new System.Drawing.Point(573, 53);
+            button3.Name = "button3";
+            button3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            button3.Size = new System.Drawing.Size(121, 81);
+            button3.TabIndex = 2;
+            button3.Text = "Araçlar";
+            button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button4.Location = new System.Drawing.Point(573, 220);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(121, 81);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Mesaj";
-            this.button4.UseVisualStyleBackColor = true;
+            button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            button4.Location = new System.Drawing.Point(573, 220);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(121, 81);
+            button4.TabIndex = 3;
+            button4.Text = "Mesaj";
+            button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button5.Location = new System.Drawing.Point(317, 220);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(121, 81);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Hesap Bilgileri";
-            this.button5.UseVisualStyleBackColor = true;
+            button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button5.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            button5.Location = new System.Drawing.Point(317, 220);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(121, 81);
+            button5.TabIndex = 4;
+            button5.Text = "Hesap Bilgileri";
+            button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button6.Location = new System.Drawing.Point(42, 220);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(121, 81);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Şoförler";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button6.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            button6.Location = new System.Drawing.Point(42, 220);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(121, 81);
+            button6.TabIndex = 5;
+            button6.Text = "Şoförler";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(593, 379);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 31);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Çıkış";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Anasayfa
             // 
@@ -105,12 +128,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gold;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(button6);
+            this.Controls.Add(button5);
+            this.Controls.Add(button4);
+            this.Controls.Add(button3);
+            this.Controls.Add(button2);
+            this.Controls.Add(button1);
             this.Name = "Anasayfa";
             this.Text = "Anasayfa";
             this.Load += new System.EventHandler(this.Anasayfa_Load);
@@ -120,11 +144,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
