89c045b0ba314a325f5c5c7a9baf74e6351d9d45
Kazim <kazimgumus032@gmail.com>
2021-06-10 17:14:51 +0300

    #17 Şoförler ekranı tasarım 2

--- Taksi Uygulamasi/Taksi Uygulamasi/Soforler.Designer.cs
+++ Taksi Uygulamasi/Taksi Uygulamasi/Soforler.Designer.cs
@@ -38,6 +38,8 @@
             this.Ehliyet = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Kayıt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -101,16 +103,36 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(134, 97);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Şoförler";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(206, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 97);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Şoför Ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(654, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Çıkış";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Soforler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Name = "Soforler";
@@ -132,5 +154,7 @@
         private System.Windows.Forms.ColumnHeader Ehliyet;
         private System.Windows.Forms.ColumnHeader Kayıt;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
