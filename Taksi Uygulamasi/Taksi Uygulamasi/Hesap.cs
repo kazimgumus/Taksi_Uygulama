55661e552ee11d42450a934cab336656784be0bf
Kazim <kazimgumus032@gmail.com>
2021-06-14 22:56:35 +0300

    #34 Hesap ekran tasarımı 2 yapıldı.

--- Taksi Uygulamasi/Taksi Uygulamasi/Hesap.cs
+++ Taksi Uygulamasi/Taksi Uygulamasi/Hesap.cs
@@ -16,5 +16,10 @@ namespace Taksi_Uygulamasi
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
